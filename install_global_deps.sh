ef8e43fd76ebb652d751fbcc72df37decdb91217
David Simic <dpsimic@gmail.com>
2014-09-23 12:59:04 -0400

    chsh

--- install_global_deps.sh
+++ install_global_deps.sh
@@ -1,7 +1,7 @@
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install python-pip git tmux vim zsh mutt
-# set zsh as default shell
-chsh -s `which zsh`
 # install virtualenvwrapper
 pip install -U virtualenvwrapper
+# set zsh as default shell
+chsh -s `which zsh`
