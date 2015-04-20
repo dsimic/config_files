347711b3c98c7f4c0bbf05527659636d27e5a2be
David Simic <dpsimic@gmail.com>
2015-04-20 17:24:04 -0400

    added user who ami to detect orig user in install_global_deps

--- install_global_deps.sh
+++ install_global_deps.sh
@@ -3,5 +3,8 @@ apt-get -y upgrade
 apt-get -y install python-pip git tmux vim zsh mutt python-dev
 # install virtualenvwrapper
 pip install -U virtualenvwrapper
+# get original user
+ORIG_USER=`who am i | awk '{print $1}'`
+echo $ORIG_USER
 # set zsh as default shell
-chsh -s `which zsh`
+chsh -s `which zsh` $ORIG_USER
