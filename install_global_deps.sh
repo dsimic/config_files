b7c944b9c5f7d3168f444ce4bfe3627a052b710c
David Simic <dpsimic@gmail.com>
2016-09-13 12:12:32 +0200

    added sudo

--- install_global_deps.sh
+++ install_global_deps.sh
@@ -1,9 +1,12 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install python-pip git tmux vim zsh mutt python-dev
+sudo apt-get -y install python-pip git tmux vim zsh mutt python-dev curl
+sudo apt-get -y install libpq-dev
+sudo apt-get -f openssh-server
 # install virtualenvwrapper
 sudo pip install -U pip
 sudo pip install -U virtualenvwrapper
+sudo pip install -U fabric
 # get original user
 ORIG_USER=`who am i | awk '{print $1}'`
 echo $ORIG_USER
---
+++ install_nvm.sh
@@ -0,0 +1,7 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | \
+    bash
+
+nvm install node
+
+
+npm install -g gulp webpack grunt bower
